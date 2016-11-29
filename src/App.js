aafc22453e20b8745f4ee3278d53ec31d006f016
Andrew Lau <an.lau23@gmail.com>
2016-11-29 23:57:12 +0000

    Fixed timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.css';
 
-export default App;
-
 class App extends React.Component {
   constructor() {
     super()
-    this.state = { current_time: new Date().toLocaleTimeString() };
-    setInterval(this.tick.bind(this), 1000);
-  };
-  element = (
-      <div> this.setState( current_time: {new Date().toLocaleTimeString()} ) </div>
-    );
+    this.state = { current_time: new Date().toLocaleTimeString() }
+    setInterval(this.tick.bind(this), 1000)
+  }
   render() {
+    const element = (
+      <div> {this.state.current_time} </div>
+    )
     return (element);
-  };
+  }
   tick() {
-    this.setState( element /*current_time: {new Date().toLocaleTimeString()} */);
-  };
-  //setInterval(tick, 1000);
-};
-
+    this.setState( {current_time: new Date().toLocaleTimeString()} );
+  }
+}
+export default App;
