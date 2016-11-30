a2d162e5a7a86dcec62d94407288647e7be14a2c
Andrew Lau <an.lau23@gmail.com>
2016-11-30 22:46:53 +0000

    Adding in mount/unmount

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = { current_time: new Date().toLocaleTimeString() }
-    setInterval(this.tick.bind(this), 1000)
   }
+
   render() {
     const element = (
-      <div> {this.state.current_time} </div>
+      <div>
+        <h1> Hello World! </h1> 
+        <h2> It is {this.state.current_time} </h2>
+      </div>
     )
     return (element);
   }
+
+  componentDidMount() {
+    var intervalId = setInterval(this.tick, 1000); //stores intervalId to be accessed later
+    this.setState({intervalId: intervalId});
+  }
+  
+  componentWillUnmount()
+  {
+    // user intervalId from state to clear the interval
+    clearInterval(this.state.intervalId);
+  }
+
   tick() {
     this.setState( {current_time: new Date().toLocaleTimeString()} );
   }
