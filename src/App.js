7517f7b33aaa0158ba1be32612d86487b9fd4b1d
Andrew Lau <an.lau23@gmail.com>
2016-12-01 23:28:58 +0000

    Minor changes to avoid redundancy, fixed timer not updating properly by binding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super()
-    this.state = { current_time: new Date().toLocaleTimeString() }
+    this.state = { current_time: new Date() }
   }
 
   render() {
     const element = (
       <div>
         <h1> Hello World! </h1> 
-        <h2> It is {this.state.current_time} </h2>
+        <h2> It is {this.state.current_time.toLocaleTimeString()} </h2>
       </div>
     )
     return (element);
   }
 
   componentDidMount() {
-    var intervalId = setInterval(this.tick, 1000); //stores intervalId to be accessed later
+    var intervalId = setInterval(this.tick.bind(this), 1000); //stores intervalId to be accessed later
     this.setState({intervalId: intervalId});
   }
   
@@ -29,7 +29,6 @@ class App extends React.Component {
   }
 
   tick() {
-    this.setState( {current_time: new Date().toLocaleTimeString()} );
+    this.setState( {current_time: new Date()} );
   }
 }
-export default App;
