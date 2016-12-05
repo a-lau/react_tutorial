106e02c4bfd60d0a81a966316dc70ae0e691a8ed
Andrew Lau <an.lau23@gmail.com>
2016-12-05 23:32:24 +0000

    Pulled out Login/out buttons for more clarity in LoginControl

--- src/LoginControl.js
+++ src/LoginControl.js
@@ -1,5 +1,7 @@
 import React from 'react'
-//import Greeting from './Greetings'
+import Greeting from './Greetings'
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton'
 
 export default class LoginControl extends React.Component {
   constructor(props) {
@@ -7,25 +9,6 @@ export default class LoginControl extends React.Component {
     this.handleLoginClick = this.handleLoginClick.bind(this);
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
     this.state = {isLoggedIn: false};
-    this.Greeting = this.Greeting.bind(this);
-    this.LoginButton = this.LoginButton.bind(this);
-    this.LogoutButton = this.LogoutButton.bind(this);
-  }
-
-  UserGreeting() {
-    return <h1>Welcome back!</h1>;
-  }
-
-  GuestGreeting() {
-    return <h1>Please sign up.</h1>;
-  }
-
-  Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-      return ( this.UserGreeting() );
-    }
-    return ( this.GuestGreeting() );
   }
 
   handleLoginClick() {
@@ -36,35 +19,19 @@ export default class LoginControl extends React.Component {
     this.setState({isLoggedIn: false});
   }
   
-  LoginButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Login
-      </button>
-    );
-  }
-
-  LogoutButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Logout
-      </button>
-    );
-  }
-
   render() {
     const isLoggedIn = this.state.isLoggedIn;
 
     let button = null;
     if (isLoggedIn) {
-      button = (<this.LogoutButton onClick={this.handleLogoutClick} />) ;
+      button = (<LogoutButton onClick={this.handleLogoutClick} />) ;
     } else {
-      button = (<this.LoginButton onClick={this.handleLoginClick} />) ;
+      button = (<LoginButton onClick={this.handleLoginClick} />) ;
     }
 
     return (
       <div>
-        <this.Greeting isLoggedIn={isLoggedIn} />
+        <Greeting isLoggedIn={isLoggedIn} />
         {button}
       </div>
     );
