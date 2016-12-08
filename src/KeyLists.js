bd8d26cea4ed8ef884b57b0ee73d68fe66d82bc3
Andrew Lau <an.lau23@gmail.com>
2016-12-08 18:03:08 +0000

    Adding in key, moving the number declaration into index.js

--- src/KeyLists.js
+++ src/KeyLists.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default class KeyLists extends React.Component {
+  constructor(props) {
+    super(props)
+    this.numbers = this.props.numbers;
+  }
   render() {
-    const numbers = [1, 2, 3, 4, 5];
-    const listItems = numbers.map((number) =>
-      <li>{number}</li>
+    const listItems = this.numbers.map((number) =>
+      <li key={number.toString()}> 
+           {number}
+      </li>
     );
     return (
       <ul>{listItems}</ul>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import KeyLists from './KeyLists';
 import './index.css';
 
+const numbers = [1, 2, 3, 4, 5];
+
 ReactDOM.render(
-  <KeyLists />,
+  <KeyLists numbers={numbers} />,
   document.getElementById('root')
 );
